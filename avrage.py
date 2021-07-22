b7de36f38acd2931a1597828d195c182d4bc92d5
KJYOTSNAA <83228398+KJYOTSNAA@users.noreply.github.com>
2021-07-22 09:39:25 +0530

    Add files via upload

---
+++ avrage.py
@@ -0,0 +1,6 @@
+sub1=int(input("enter the first subject no:"))
+sub2=int(input("enter the second subject no:"))
+sub3=int(input("enter the third subject no:"))
+
+avg=int((sub1+sub2+sub3)/3)
+print("avrage:",avg)
