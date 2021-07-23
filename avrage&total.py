9b4c7d5ae7539d0b3250934bcee37ab5fcbc3432
K. JYOTSNA <83228398+KJYOTSNAA@users.noreply.github.com>
2021-07-23 23:33:00 +0530

    Create avrage&total.py

---
+++ avrage&total.py
@@ -0,0 +1,7 @@
+sub1=int(input("enter the first subject no:"))
+sub2=int(input("enter the second subject no:"))
+sub3=int(input("enter the third subject no:"))
+t=sub1+sub2+sub3
+avg=int((t)/3)
+print("TOTAL:",t)
+print("avrage:",avg)
