18afe1767fa6fbe296049c96339a4068c212dba0
K. JYOTSNA <83228398+KJYOTSNAA@users.noreply.github.com>
2021-07-23 23:34:04 +0530

    Create resistance.py

---
+++ resistance.py
@@ -0,0 +1,8 @@
+r1=int(input("enter the resister1 value:"))
+r2=int(input("enter the resister2 value:"))
+
+s=r1+r2
+p=(r1*r2)/(r1+r2)
+
+print("resistance of circuit in serious:",s)
+print("resistance of circuit in parallel:",p)
